5bb480a5cfb8f90c770eb6027fe967a19f69e2a5
dunkeln <prateekpravanjan@gmail.com>
2024-04-04 01:55:21 -0400

    bolsters 🦴

---
+++ notebooks/scripts/lib.py
@@ -0,0 +1,18 @@
+from functools import reduce
+import matplotlib.image as mpimg
+import numpy as np
+import os
+
+def compose(*fns):
+    def __inner__(x):
+        return reduce(lambda acc, fn: fn(acc), fns, x)
+    return __inner__
+
+img_to_matrix = compose(mpimg.imread, np.asarray)
+
+read_dir = lambda path: map(
+    img_to_matrix, compose(
+        os.listdir,
+        lambda x: map(lambda file: os.path.join(path, file), x)
+    )(path)
+)
