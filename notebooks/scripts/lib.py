e3d04b1ae6be5d6b83e99e858541e887cc233838
dunkeln <46231179+dunkeln@users.noreply.github.com>
2024-04-05 08:16:26 +0000

    [CHKPNT] resolved all data loading issues

--- notebooks/scripts/lib.py
+++ notebooks/scripts/lib.py
@@ -1,6 +1,7 @@
 from functools import reduce
 import matplotlib.image as mpimg
-import numpy as np
+import torch
+import polars as pl
 import os
 
 def compose(*fns):
@@ -8,13 +9,11 @@ def compose(*fns):
         return reduce(lambda acc, fn: fn(acc), fns, x)
     return __inner__
 
-img_to_matrix = compose(mpimg.imread, np.asarray)
-
-read_dir = lambda path: map(
-    img_to_matrix, compose(
-        os.listdir,
-        lambda x: map(lambda file: os.path.join(path, file), x)
-    )(path)
-)
+img_to_matrix = compose(mpimg.imread, torch.tensor)
 
 get_size = compose(os.listdir, list.__len__)
+
+create_df = lambda path: pl.DataFrame({
+    'file': (fake_faces := os.listdir(path + 'fake')) + (real_faces := os.listdir(path + 'real')),
+    'label': [ 0 for _ in range(len(fake_faces))] + [1 for _ in range(len(real_faces)) ],
+})
